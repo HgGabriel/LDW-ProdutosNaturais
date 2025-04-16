69f4bdf3d0f32a01c9095aedc1af6d408936e330
HgGabriel <hiagogabrieloliveirapinto@gmail.com>
2025-04-16 19:10:09 -0300

    adicionando novos componentes[

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,11 +8,9 @@ import { shuffleArray } from "../../utils";
 import { product } from "../../types";
 
 const Home: React.FC = () => {
-  const { trendingCategories, currentCategory } =
-    useAppContext();
+  const { trendingCategories, currentCategory } = useAppContext();
   const [currentSubCategory, setCurrentSubCategory] =
     React.useState<string>("");
-
 
   const [promoProducts, setPromoProducts] = React.useState<product[]>([]);
 
@@ -38,36 +36,45 @@ const Home: React.FC = () => {
               }}
               key={index}
               className={styles.categoryItem}
+              style={
+                currentSubCategory === category.name
+                  ? { borderBottom: "2px solid #000" }
+                  : {}
+              }
             >
               <img src={category.image} alt={category.name} />
               <p>{category.name}</p>
             </div>
           ))}
       </div>
-      <div className={styles.productsList}>
-        {categoryData[currentCategory]?.products
-          .filter(
-            (product) =>
-              !currentSubCategory ||
-              product.subCategory.toLowerCase() ===
-                currentSubCategory.toLowerCase()
-          )
-          .slice(0, 5)
-          .map((product, index) => (
-            <ProductCard key={index} product={product} />
-          ))}
-      </div>
-      <div>
-        <span>Ver mais</span>
+      <div className={styles.productsContainer}>
+        <div className={styles.productsList}>
+          {categoryData[currentCategory]?.products
+            .filter(
+              (product) =>
+                !currentSubCategory ||
+                product.subCategory.toLowerCase() ===
+                  currentSubCategory.toLowerCase()
+            )
+            .slice(0, 5)
+            .map((product, index) => (
+              <ProductCard key={index} product={product} />
+            ))}
+        </div>
+        <div className={styles.seeMore}>
+          <span>Ver mais</span>
+        </div>
       </div>
       <h1>Promoções</h1>
-      <div className={styles.productsList}>
-        {promoProducts.slice(0, 5).map((product, index) => (
-          <ProductCard key={index} product={product} />
-        ))}
-      </div>
-      <div>
+      <div className={styles.productsContainer}>
+        <div className={styles.productsList}>
+          {promoProducts.slice(0, 5).map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
+        </div>
+      <div className={styles.seeMore}>
         <span>Ver mais</span>
+      </div>
       </div>
     </div>
   );
